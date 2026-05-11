0c5ab054795c0e67f133a6c01951f740513d7412
DinoPimenteiro <gabrielwilliam234@gmail.com>
2026-05-11 16:33:17 -0300

    create components for initial page

--- src/features/product/components/Drops.tsx
+++ src/features/product/components/Drops.tsx
@@ -4,17 +4,20 @@ import img1 from "@/assets/models/image.png";
 import img2 from "@/assets/models/urbn_model_female_2.png";
 import img3 from "@/assets/models/urbn_model_male_1.png";
 import img4 from "@/assets/models/urbn_model_male_2.png";
+import { Span } from "@/shared/components/atoms/Span";
 
 const Drops = () => {
   return (
     <section>
-      <h2>Lançamentos</h2>
-      <div>
-        <span>
-          VIEW ALL <ArrowRight />
-        </span>
-      </div>
-      <div className="flex flex-row ">
+      <header className="flex justify-between px-8 my-5">
+        <h2 className="text-xl font-bold text-text-primary">Lançamentos</h2>
+        <div>
+          <Span>
+            VIEW ALL <ArrowRight />
+          </Span>
+        </div>
+      </header>
+      <div className="flex flex-col gap-3 px-8">
         <Card text={"Urban fit shirt"} price={"R$78,90"} image={img1} />
         <Card text={"Urban fit shirt"} price={"R$78,90"} image={img2} />
         <Card text={"Urban fit shirt"} price={"R$78,90"} image={img3} />
--- src/features/product/components/HeroBanner.tsx
+++ src/features/product/components/HeroBanner.tsx
@@ -1,15 +1,34 @@
 import { Button } from "@/shared/components/atoms/Button";
 import heroImage from "@/assets/models/hero_image.png";
+import crownImage from "@/assets/icons/urbn_crown_icon.png";
+import { Globe } from "lucide-react";
+import { Span } from "@/shared/components/atoms/Span";
+import { SubTitle } from "@/shared/components/atoms/SubTitle";
 
 export const HeroBanner = () => {
   return (
-    <section>
-      <span>Built different. </span>
-      <h1>MADE TO STAND OUT.</h1>
-      <p>StreetWear that speaks louder than words. No rules. No limits.</p>
-      <Button color="primary">Shop Now</Button>
-      <div className="relative">
-        <img src={heroImage} alt="" className="w-full" />
+    <section className="text-text-primary text-xl w-full relative overflow-hidden p-8 bg-radial-[at_25%_25%] from-primary to-accent-blue/20 to-85% ">
+      <div className="z-10 relative w-3/4 flex flex-col gap-5">
+        <SubTitle>
+          BUILT DIFFERENT. <Globe />
+        </SubTitle>
+        <h1 className="text-4xl font-bold text-accent-pink">
+          MADE TO STAND OUT.
+        </h1>
+        <Span>StreetWear that speaks louder than words.</Span>
+        <Span>No rules. No limits.</Span>
+        <div>
+          <Button color="primary">Shop Now</Button>
+        </div>
+      </div>
+
+      <div className="absolute w-full -right-30 top-0 opacity-70">
+        <img src={heroImage} alt="" className="w-full h-full object-cover" />
+        <img
+          src={crownImage}
+          alt=""
+          className="w-1/4 object-cover absolute top-0 right-90 -rotate-45"
+        />
       </div>
     </section>
   );
